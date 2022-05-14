8a3f6cc5dec0fd198d901e10a5549aed7f7a251b
Finn <GeeWizWow@users.noreply.github.com>
2022-05-14 13:18:06 +0100

    feat: removed redundant entities, logic, and controls

--- constants.lua
+++ constants.lua
@@ -1,3 +1,3 @@
 --GRAPHICSPATH = "__cargo-ships__/graphics/"
-GRAPHICSPATH = "__cargo-ships-graphics__/graphics/"
+GRAPHICSPATH = "__oversea-energy-distribution-graphics__/graphics/"
 emptypic = { filename = GRAPHICSPATH .. "blank.png", size = 1, direction_count = 1, variation_count = 1 }
--- data-updates.lua
+++
@@ -1,4 +0,0 @@
-require("constants")
-if settings.startup["deep_oil"].value then
-  require("prototypes.resources")
-end
